65c45b105afef376bb8dd09c1b8210d0ff5193ed
acgeusmile <acgeusmile@gmail.com>
2019-10-28 23:50:36 -0600

    Improve the readability of txt_generator

--- src/node/txt_generator.py
+++ src/node/txt_generator.py
@@ -3,13 +3,11 @@ import json
 import os
 import glob
 from xml.etree.ElementTree import ElementTree, Element
+# import self package library
+sys.path.append('/workspace/yo/google_od_api/src/lib/')
+from utility import load_config
 
-RESULT_PATH = './output_b4_with_momentum/bdd100k_test.json'
-IMAGESETS_PATH = '/workspace/datasets/BDD100k/bdd100k/images/100k/test/'
-ANNOTATIONS_PATH= '/workspace/datasets/BDD100k/VOC_test/Annotations/'
-#RESULT_PATH = './output/result_annos_sim.json'
-#IMAGESETS_PATH = '/workspace/datasets/BDD100k/VOC_simple/ImageSets/test.txt'
-#ANNOTATIONS_PATH= '/workspace/datasets/BDD100k/VOC_simple/Annotations/'
+cfg = load_config.readCfg("cfg/txt_generator.json")
 
 def del_file(name:str):
     if os.path.exists(name):
@@ -34,10 +32,10 @@ def readXml(name:str):
     return tree.getroot()
 
 def detection():
-    with open(RESULT_PATH, 'r') as json_fd:
+    with open(cfg["DETECTION_PATH"], 'r') as json_fd:
         info = json.load(json_fd)
         for name in list(info['imgs'].keys()):
-            file_name = "".join(('./predict_results/detections/',name.split('.')[0],'.txt'))
+            file_name = "".join((cfg["OUTPUT_PATH"], '/detections/',name.split('.')[0],'.txt'))
             del_file(file_name)
             for i in range(len(info['imgs'][name]['objects'])):
                 category = info['imgs'][name]['objects'][i]['category']
@@ -51,31 +49,32 @@ def detection():
                     predict_fd.write(string)
 
 def label():
-    with open(IMAGESETS_PATH, 'r')as imageset_fd:
-        for line in imageset_fd:
-            name = line[:-1]
-            file_name = "".join(('./predict_results/groundtruths/',name,'.txt'))
-            del_file(file_name)
-            with open("".join((ANNOTATIONS_PATH, name, '.xml')), 'r') as label_fd:
-                xml_root = readXml(label_fd)
-                for m in xml_root.findall('object'):
-                    category = m.find('name').text
-                    bd_obj = m.find('bndbox')
-                    bd_info = getIter(bd_obj)
-                    with open(file_name, "a") as label_fd:
-                        string = " ".join((category, bd_info['xmin'],
-                            bd_info['ymax'], bd_info['xmax'], bd_info['ymin'],'\n'))
-                        label_fd.write(string)
+    for index, image_path in enumerate(glob.glob(cfg["DIR_IMAGE"]+'/*.jpg')):
+        name = image_path.rstrip().split('/')[-1]
+        file_name = "".join((cfg["OUTPUT_PATH"], '/groundtruths/',image_id,'.txt'))
+        del_file(file_name)
+        with open("".join((cfg["ANNOTATIONS_PATH"], name, '.xml')), 'r') as label_fd:
+            xml_root = readXml(label_fd)
+            for m in xml_root.findall('object'):
+                category = m.find('name').text
+                bd_obj = m.find('bndbox')
+                bd_info = getIter(bd_obj)
+                with open(file_name, "a") as label_fd:
+                    string = " ".join((category, bd_info['xmin'],
+                        bd_info['ymax'], bd_info['xmax'], bd_info['ymin'],'\n'))
+                    label_fd.write(string)
 
-def mkdir(directory):
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-    else:
-        pass
+def mkdir(*directories):
+    for directory in list(directories):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        else:
+            pass
 
 def main():
-    mkdir('predict_results')
-    #label()
+    mkdir("".join((cfg["OUTPUT_PATH"], '/groundtruths/')),
+          "".join((cfg["OUTPUT_PATH"], '/detections/')))
+    label()
     detection()
 
 if __name__ == "__main__":
