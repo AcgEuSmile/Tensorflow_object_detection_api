31cef062dd3470140393bbbc82c0a32ad3886976
acgeusmile <acgeusmile@gmail.com>
2019-11-05 23:48:53 -0700

    Add some if statement

--- scripts/cp_img.bash
+++ scripts/cp_img.bash
@@ -1,15 +1,16 @@
-# read input
-read -p "Please input target directory: " PATH_NAME
-# if "./${PATH_NAME}" exist, then 
+read -p "please input target directory: " PATH_NAME
 if [ -d "./${PATH_NAME}" ]; then
-  # if "./JPEGImages_${PATH_NAME}" doesn't exit, then
-  if [ ! -d  "./JPEGImages_${PATH_NAME}"  ]; then
-    # make directory
+  if [ -d  "./JPEGImages_${PATH_NAME}"  ]; then
+    read -p "The output folder is exist, do you want to replace it?(Y/n) " REPLACE
+    if [ ${REPLACE} == "Y" ] || [ ${REPLACE} == "y" ]; then
+      find ./${PATH_NAME}/ -name "*.jpg"  -type f -print0 | xargs -0 -i cp {} ./JPEGImages_${PATH_NAME}
+    else
+      echo "Give up replacing"
+    fi
+  else
     mkdir -p JPEGImages_${PATH_NAME}
-    # find all jpg "files" and copy to "./JPEGImages_${PATH_NAME}"
     find ./${PATH_NAME}/ -name "*.jpg"  -type f -print0 | xargs -0 -i cp {} ./JPEGImages_${PATH_NAME}
   fi
 else
-  # if "./${PATH_NAME}" exist, then show error message.
   echo "./${PATH_NAME} directory not found"
 fi
